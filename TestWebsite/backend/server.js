a1275f1b18dee12546fde0df5c03b61dc62b0815
vansh-bit <sharmavansh197900@gmail.com>
2026-06-05 20:01:35 +0530

    fix CORS for production

--- TestWebsite/backend/server.js
+++ TestWebsite/backend/server.js
@@ -12,7 +12,7 @@ const contentRoutes = require('./routes/content');
 const app = express();
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+  origin: ['https://website-analytics-dashboard-seven.vercel.app', 'http://localhost:5500'],
   methods: ['GET', 'POST', 'PUT', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
